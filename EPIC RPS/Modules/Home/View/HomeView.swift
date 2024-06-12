59b26029b74be3a21700d6142612537818d35d32
Nikita Kornuk <kornyk62@gmail.com>
2024-06-12 22:00:49 +0200

    add NavigationView

--- EPIC RPS/Modules/Home/View/HomeView.swift
+++ EPIC RPS/Modules/Home/View/HomeView.swift
@@ -9,78 +9,87 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        VStack {
-            HStack {
-                Button {
-                    // action
-                } label: {
-                    Image(systemName: "gearshape")
-                        .resizable()
-                        .frame(width: 35, height: 35)
-                        .foregroundColor(Color.black)
-                }
-                Spacer()
-                Button {
-                    // action
-                } label: {
-                    Image(systemName: "questionmark.circle.fill")
-                        .resizable()
-                        .frame(width: 35, height: 35)
-                        .foregroundColor(Color.black)
-                }
-            }
-            .padding(.horizontal, 21)
-            Spacer()
-            VStack(spacing: 55){
-                HStack {
-                    Spacer()
-                    Image("maleHandInverse")
-                }
-                ZStack {
-                    Text("EPIC RPS")
-                        .font(.system(size: 30, weight: .bold))
-                        .foregroundColor(Color.orange.opacity(0.5))
-                        .offset(x: 1.5, y: 1.5)
-                    Text("EPIC RPS")
-                        .font(.system(size: 30, weight: .bold))
-                        .foregroundColor(Color.orange)
-                }
-                HStack {
-                    Image("femaleHandInverse")
-                    Spacer()
-                }
-            }
-            Spacer()
-            VStack(spacing: 11){
-                ZStack{
-                    Button {
-                        // action
-                    } label: {
-                        Image("backgroundButton")
+        NavigationView {
+            VStack {
+                VStack{
+                    Text("")
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarLeading) {
+                                Button(action: {
+                                    print("Left button tapped")
+                                }) {
+                                    Image(systemName: "gearshape")
+                                        .resizable()
+                                        .frame(width: 35, height: 35)
+                                        .foregroundColor(Color.black)
+                                }
+                            }
                             
-                    }
-                    Text("MAIN")
-                        .bold()
-                        .font(.system(size: 16))
-                        .foregroundStyle(Color.brown)
-                        .offset(y:-4)
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                Button(action: {
+                                    print("Right button tapped")
+                                }) {
+                                    Image(systemName: "questionmark.circle.fill")
+                                        .resizable()
+                                        .frame(width: 35, height: 35)
+                                        .foregroundColor(Color.black)
+                                }
+                            }
+                        }
                 }
                 
-                ZStack{
-                    Button {
-                        // action
-                    } label: {
-                        Image("backgroundButton")
-                            
+                Spacer()
+                VStack(spacing: 55){
+                    HStack {
+                        Spacer()
+                        Image("maleHandInverse")
                     }
-                    Text("RESULT")
-                        .bold()
-                        .font(.system(size: 16))
-                        .foregroundStyle(Color.brown)
-                        .offset(y:-4)
+                    ZStack {
+                        Text("EPIC RPS")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundColor(Color.orange.opacity(0.5))
+                            .offset(x: 1.5, y: 1.5)
+                        Text("EPIC RPS")
+                            .font(.system(size: 30, weight: .bold))
+                            .foregroundColor(Color.orange)
+                    }
+                    HStack {
+                        Image("femaleHandInverse")
+                        Spacer()
+                    }
                 }
+                Spacer()
+                VStack(spacing: 11){
+                    ZStack{
+                        Button {
+                            // action
+                        } label: {
+                            Image("backgroundButton")
+                            
+                        }
+                        Text("MAIN")
+                            .bold()
+                            .font(.system(size: 16))
+                            .foregroundStyle(Color.brown)
+                            .offset(y:-4)
+                    }
+                    
+                    ZStack{
+                        Button {
+                            // action
+                        } label: {
+                            Image("backgroundButton")
+                            
+                        }
+                        Text("RESULT")
+                            .bold()
+                            .font(.system(size: 16))
+                            .foregroundStyle(Color.brown)
+                            .offset(y:-4)
+                    }
+                }
+                Spacer()
             }
-            Spacer()
         }
     }
 }
